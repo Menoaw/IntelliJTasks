6d271090b0e88477cf29be9007c7d2f908545264
Matthew K <wabasi333@gmail.com>
2021-07-30 05:32:29 -0500

    Updates

--- Practice/Randoms.java
+++ Practice/Randoms.java
@@ -2,20 +2,17 @@ package Practice;
 
 public class Randoms {
     public static void main(String[] args) { //
+                                           //           01234
+        int count = 0;
+        for (int a = 0; a<4; a++){
+            if (a==3) continue;
+            for (int b = a +1; b < 5; b++){
+                count++;
+                if (b == 3) break;
 
-String str = "cybertek";
-
-        for (int i = 0; i < str.length() ; i+=2) {
-            System.out.print(str.charAt(i));
-
+            }
         }
-        for (int i = 0; i <=str.length()-1 ; i+=2) {
-            System.out.print(str.charAt(i));
-
-        }
-
-
-
+        System.out.println(count);
 
 
     }
